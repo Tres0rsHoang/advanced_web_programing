01273fa96251d5e91ab7883602b46326a42ee880
Bảo Hoàng <74838783+Tres0rsHoang@users.noreply.github.com>
2023-11-14 00:33:14 +0700

    Refactor: update package

--- be/index.js
+++ be/index.js
@@ -2,10 +2,8 @@ import express from 'express';
  
 const app = express();
 
-const port =  9000;
-
 app.get('/', (req, res) => {
     res.json({"message" : "sucess"});
 });
 
-app.listen(port, () => {console.log("Server is running at " + port)});
+export default app;
