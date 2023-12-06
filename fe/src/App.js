75ef0ec0f26591e29fa5b62c4e9b55b03e6cfd1d
Trương Trọng Khánh <82425500+trongkhanh11@users.noreply.github.com>
2023-12-06 23:28:01 +0700

    update app.js

--- fe/src/App.js
+++ fe/src/App.js
@@ -9,22 +9,48 @@ import Products from './Pages/products';
 import Profile from './Pages/profile';
 import ResetPassword from './Pages/resetPassword';
 import SignUp from './Pages/signUp';
+import { ToastContainer } from 'react-toastify';
+import { useContext, useEffect } from 'react';
+import { UserContext } from './context/userContext';
 
 function App() {
+  const { loginContext } = useContext(UserContext);
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  useEffect(() => {
+    if(localStorage.getItem("access_token")) {
+      loginContext(user.email, localStorage.getItem("access_token"))
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/landing' element={<Landing />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signUp' element={<SignUp />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/aboutUs' element={<AboutUs />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/resetPassword' element={<ResetPassword />} />
-        <Route path='/reset-password' element={<ConfirmResetPassword />} />
-      </Routes>
-    </div>
+    <>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/landing' element={<Landing />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signUp' element={<SignUp />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/aboutUs' element={<AboutUs />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/resetPassword' element={<ResetPassword />} />
+          <Route path='/reset-password' element={<ConfirmResetPassword />} />
+        </Routes>
+      </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
   );
 }
 
