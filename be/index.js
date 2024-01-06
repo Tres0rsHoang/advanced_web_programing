fd634f17a9068edad926d4ab747043590b54feee
Bảo Hoàng <74838783+Tres0rsHoang@users.noreply.github.com>
2024-01-06 23:28:11 +0700

    Update: change server port

--- be/index.js
+++ be/index.js
@@ -45,7 +45,7 @@ app.get('/', function (req, res, next) {
     res.send("Server is running...");
 });
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 9000);
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
 });
